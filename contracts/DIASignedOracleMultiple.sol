a1110ee6cc94a9c879af4044a7cb4cfacbfe7976
donoso.eth <30406536+donoso-eth@users.noreply.github.com>
2024-10-16 16:21:38 +0200

    Update DIASignedOracleMultiple.sol
    Updated following  https://github.com/diadata-org/diadata/blob/master/pkg/dia/scraper/blockchain-scrapers/blockchains/ethereum/diaOracleV2Multiupdate/DIASignedOracleMultiple.sol

--- contracts/DIASignedOracleMultiple.sol
+++ contracts/DIASignedOracleMultiple.sol
@@ -2,20 +2,19 @@
 
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DIASignedOracleMultiple is Ownable {
-    using EnumerableSet for EnumerableSet.AddressSet;
-    EnumerableSet.AddressSet private _oracleUpdaters;
-
-    string name = "DiaData";
-    string version = "1.0.0";
-    uint256 chainId = 1;
-    address verifyingContract = 0x0000000000000000000000000000000000000000;
     address public signer;
-
     struct Message {
+        string Symbol;
+        address Address;
+        string Blockchain;
+        uint256 Price;
+        uint256 Time;
+    }
+
+    struct RawMessage {
         string Symbol;
         address Address;
         string Blockchain;
@@ -27,28 +26,37 @@ contract DIASignedOracleMultiple is Ownable {
     mapping(string => Message) public values;
 
     event OracleUpdate(string key, uint256 value, uint256 timestamp);
-    event UpdaterAddressChange(address newUpdater);
-    event RequestUpdate(
+ 
+     event RequestUpdate(
         address assetAddress,
         string blockchain,
         uint256 timestamp
     );
 
     constructor() Ownable(msg.sender) {
-        _oracleUpdaters.add(msg.sender);
         signer = msg.sender;
     }
 
-    function setValues(Message[] memory vs) public {
+    function setValues(RawMessage[] memory vs) public {
         for (uint i = 0; i < vs.length; i++) {
             this.setValue(vs[i]);
         }
     }
 
-    function setValue(Message memory value) external {
-        require(getSigner(value) == signer, "SIGNER_DONT_MATCH");
-        values[value.Symbol] = value;
-        emit OracleUpdate(value.Symbol, value.Price, value.Time);
+    function setValue(RawMessage memory v) external {
+        require(getSigner(v) == signer, "SIGNER_DONT_MATCH");
+        require(v.Time >= values[v.Symbol].Time, "NEWER_TIMESTAMP_REQUIRED");
+
+        Message memory valueWithoutSignature = Message({
+            Symbol: v.Symbol,
+            Address: v.Address,
+            Blockchain: v.Blockchain,
+            Price: v.Price,
+            Time: v.Time
+        });
+
+        values[v.Symbol] = valueWithoutSignature;
+        emit OracleUpdate(v.Symbol, v.Price, v.Time);
     }
 
     function requestForUpdate(
@@ -69,21 +77,11 @@ contract DIASignedOracleMultiple is Ownable {
         signer = _newSigner;
     }
 
-    function addUpdater(address oracleUpdaterAddress) external onlyOwner {
-        _oracleUpdaters.add(oracleUpdaterAddress);
-    }
-
-    function removeUpdater(address oracleUpdaterAddress) external onlyOwner {
-        _oracleUpdaters.remove(oracleUpdaterAddress);
-    }
-
     function changeOwner(address newOwner) external onlyOwner {
-        _oracleUpdaters.add(newOwner);
-        _oracleUpdaters.remove(owner());
         _transferOwnership(newOwner);
     }
 
-    function getSigner(Message memory message) public view returns (address) {
+    function getSigner(RawMessage memory message) public pure returns (address) {
         bytes memory _signature;
         _signature = message.Signature;
 
@@ -95,10 +93,10 @@ contract DIASignedOracleMultiple is Ownable {
         bytes32 DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256(abi.encodePacked(EIP712_DOMAIN_TYPE)),
-                keccak256(abi.encodePacked(name)),
-                keccak256(abi.encodePacked(version)),
-                chainId,
-                verifyingContract
+                keccak256(abi.encodePacked("DiaData")),
+                keccak256(abi.encodePacked("1.0.0")),
+                1,
+                0x0000000000000000000000000000000000000000
             )
         );
 
